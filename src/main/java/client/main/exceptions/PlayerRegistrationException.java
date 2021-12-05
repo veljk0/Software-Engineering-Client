c8cf0e6cef356b9193c7e6b2f26525a873ea6134
Radunovic Veljko <a01528243@unet.univie.ac.at>
2021-12-05 17:11:13 +0100

    add rest exceptions

---
+++ src/main/java/client/main/exceptions/PlayerRegistrationException.java
@@ -0,0 +1,16 @@
+package client.main.exceptions;
+
+public class PlayerRegistrationException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public PlayerRegistrationException(String message) {
+		super(message);
+	}
+
+}
+
+
